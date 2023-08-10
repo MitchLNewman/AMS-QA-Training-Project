68693c35450ef5ccb52ff4c0923bde596d41a879
Mitch L Newman <mitchellnewman3@gmail.com>
2023-08-10 11:05:22 +0100

    added example commit to test if create.py works

--- create.py
+++ create.py
@@ -10,7 +10,7 @@ with app.app_context():
 
     from application.models import Product
 
-    phone = Product(name='Phone', price=500, description='A nice phone', category='Electronics')
+    phone = Product(name='Mobile Phone', price=500, description='Android', category='Electronics')
     db.session.add(phone)
     db.session.commit()
     print (phone)
