68f342fe09a4ab4176059aef1949e42378d92cdb
Mitch L Newman <mitchellnewman3@gmail.com>
2023-08-14 16:36:17 +0100

    updates

--- application/models.py
+++ application/models.py
@@ -1,186 +1,192 @@
-from application import db
-from wtforms.validators import ValidationError
+from flask import render_template, request, redirect, url_for, session
+from application import app, db
+from application.models import Product, Category, User, Orders, OrderItem, Cart, CartItem, WishList, CartDisplay, PaymentDetails
+from application.forms import SignUpForm, LoginForm, PaymentForm
+from flask_bcrypt import Bcrypt
+from application import bcrypt
 from datetime import datetime
 
-# category class
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html', title='Home')
 
-class Category(db.Model):
-    category_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    products = db.relationship('Product', backref='category', lazy=True)
+# Product related routes
 
-    def __repr__(self):
-        return ''.join([
-            'Category ID: ', str(self.id), '\r\n',
-            'Name: ', self.name
-        ])
+@app.route('/products', methods=['GET', 'POST'])
+def products():
+    products = Product.query.all()
+    categories = Category.query.all()
+    for product in products:
+        print(product)
+    return render_template('products.html', title='Products', products=products)
 
-# Product class
+@app.route('/product/<int:id>', methods=['GET', 'POST'])
+def product(id):
+    product = Product.query.get(id)
+    category = Category.query.get(product.category_id)
+    return render_template('product.html', title='Product', product=product, category=category)
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    description = db.Column(db.String(100), nullable=False)
-    image = db.Column(db.String(100), nullable=False, default='default.jpeg')
-    category_id = db.Column(db.Integer, db.ForeignKey('category.category_id'), nullable=False)
+# About and category related routes 
 
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html', title='About')
 
-    def __repr__(self):
-        return ''.join([
-            'Product ID: ', str(self.id), '\r\n',
-            'Name: ', self.name, '\r\n', self.description
-        ])
+@app.route('/category', methods=['GET', 'POST'])
+def contact():
+    categories = Category.query.all()
+    products = Product.query.all()
+    return render_template('category.html', title='Categories', categories=categories, products=products)
 
-# Payment and user related classes
+#Cart related routes
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    address = db.Column(db.String(100), nullable=False)
-    postcode = db.Column(db.String(10), nullable=False)
-    phone = db.Column(db.String(15), nullable=False)
-    orders = db.relationship('Orders', backref='customer', lazy=True)
-
-    def __repr__(self):
-        return ''.join([
-            'User ID: ', str(self.id), '\r\n',
-            'Email: ', self.email, '\r\n', self.name
-        ])
-    
-class PaymentDetails(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    card_number = db.Column(db.String(16), nullable=False)
-    expiry_date = db.Column(db.String(5), nullable=False)
-    cvv = db.Column(db.String(3), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return ''.join([
-            'PaymentDetails ID: ', str(self.id), '\r\n',
-            'User ID: ', str(self.user_id), '\r\n', str(self.card_number)
-        ])
-
-# Orders and cart related classes
-
-class Orders(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    PaymentDetails = db.relationship('PaymentDetails', backref='order', lazy=True)
-    payment_details_id = db.Column(db.Integer, db.ForeignKey('payment_details.id'), nullable=False)
-
-    def __repr__(self):
-        return ''.join([
-            'Order ID: ', str(self.id), '\r\n',
-            'User ID: ', str(self.user_id), '\r\n', str(self.date)
-        ])
-    
-class OrderItem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    quantity = db.Column(db.Integer, nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-    order = db.relationship('Orders', backref='order', lazy=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
-
-    def __repr__(self):
-        return ''.join([
-            'OrderItem ID: ', str(self.id), '\r\n',
-            'Order ID: ', str(self.order_id), '\r\n', str(self.quantity)
-        ])
-
-class Cart(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return ''.join([
-            'Cart ID: ', str(self.id), '\r\n',
-            'Product ID: ', str(self.product_id)
-        ])
-    
-    def set_quantity(self, product_id, quantity):
-        cart_item = CartItem.query.filter_by(product_id=product_id, cart_id=self.id).first()
-        if cart_item:
-            if quantity > 0:
-                cart_item.quantity = quantity
-                db.session.commit()
-            else:
-                db.session.delete(cart_item)
-                db.session.commit()
-    
-    def add_item(self, product_id):
-        cart_item = CartItem.query.filter_by(product_id=product_id, cart_id=self.id).first()
-        if cart_item:
-            cart_item.quantity += 1
-            db.session.commit()
-        else:
-            new_cart_item = CartItem(product_id=product_id, quantity=1, cart_id=self.id)
-            db.session.add(new_cart_item)
-            db.session.commit()
-    
-    def remove_item(self, product_id):
-        cart_item = CartItem.query.filter_by(product_id=product_id, cart_id=self.id).first()
-        if cart_item:
-            db.session.delete(cart_item)
-            db.session.commit()
-
-    def empty_cart(self):
-        cart_items = CartItem.query.filter_by(cart_id=self.id).all()
+@app.route('/cart', methods=['GET', 'POST'])
+def cart():
+    if 'user_id' in session:
+        cart = Cart.query.filter_by(user_id=session['user_id']).first()
+        cart_items = CartItem.query.filter_by(cart_id=cart.id).all()
+        cart_products = []
         for item in cart_items:
-            db.session.delete(item)
+            product = Product.query.get(item.product_id)
+            item = CartDisplay(product.id, product.name, product.price, item.quantity, product.image)
+            cart_products.append(item)
+        return render_template('/cart.html', title='Cart', products=cart_products)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/cart/update/<int:id>/<int:quantity>', methods=['GET', 'POST'])
+def update_cart(id, quantity):
+    if 'user_id' in session:
+        cart = Cart.query.filter_by(user_id=session['user_id']).first()
+        cart.set_quantity(id, quantity)
+        return redirect(url_for('cart'))
+
+@app.route('/add/<int:id>', methods=['GET', 'POST'])
+def add_to_cart(id):
+    if 'user_id' in session:
+        cart = Cart.query.filter_by(user_id=session['user_id']).first()
+        cart.add_item(id)
+        return redirect(url_for('cart'))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/cart/remove/<int:id>', methods=['GET', 'POST'])
+def remove_from_cart(id):
+    cart = Cart.query.filter_by(user_id=session['user_id']).first()
+    cart.remove_item(id)
+    return redirect(url_for('cart'))
+
+@app.route('/empty-cart', methods=['GET', 'POST'])
+def empty_cart():
+    cart = Cart.query.filter_by(user_id=session['user_id']).first()
+    cart.empty_cart()
+    return redirect(url_for('cart'))
+
+# Signup and Login related routes
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            print('validated')
+            user = User(
+                name = form.name.data,
+                email = form.email.data,
+                password = bcrypt.generate_password_hash(form.password.data),
+                address = form.address.data,
+                postcode = form.postcode.data,
+                phone = form.phone.data)
+
+            db.session.add(user)
             db.session.commit()
+            return redirect(url_for('home'))
+        else:
+            print('not validated')
+    return render_template('/signup.html', title='Sign Up', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        print('post')
+        if form.validate_on_submit():
+            print('validated')
+            user = User.query.filter_by(email=form.email.data).first()
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
+                print('user found')
+                session['user_id'] = user.id
+                cart = Cart.query.filter_by(user_id=user.id).first()
+                if cart:
+                    print('cart found')
+                else:
+                    print('cart not found')
+                    cart = Cart(user_id=user.id)
+                    db.session.add(cart)
+                    db.session.commit()
+                return redirect(url_for('home'))
+            else:
+                print('user not found')
+        else:
+            print('not validated')
+    return render_template('/login.html', title='Login', form=form)
+
+@app.route('/logout')
+def clear_variable():
+    session.pop('user_id', None)  # Remove 'user_id' from session
+    print("Session variable cleared!")
+    return redirect(url_for('home'))
+
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+    if 'user_id' in session:
+        # if form has been submitted
+        if request.method == 'POST':
+            print('post')
+            # check if payment details already exist
+            if PaymentDetails.query.filter_by(user_id=session['user_id'], card_number=request.form['card_number']).first():
+                payment_details = PaymentDetails.query.filter_by(user_id=session['user_id'], card_number=request.form['card_number']).first()
+            else:
+                payment_details = PaymentDetails(
+                    user_id = session['user_id'],
+                    card_number = request.form['card_number'],
+                    expiry_date = request.form['expiry_date'],
+                    cvv = request.form['security_code'])
+            db.session.add(payment_details)
+            db.session.commit()
+            # create order
+            cart = Cart.query.filter_by(user_id=session['user_id']).first()
+            payment_details_id = PaymentDetails.query.filter_by(user_id=session['user_id'], card_number=request.form['card_number']).first().id
+            cart_items = CartItem.query.filter_by(cart_id=cart.id).all()
+            order = Orders(user_id=session['user_id'], date=datetime.now(), payment_details_id=payment_details_id)
+            db.session.add(order)
+            db.session.commit()
+            # get the order id from the order just created
+            order_id = Orders.query.filter_by(user_id=session['user_id'], payment_details_id=payment_details_id).first().id
+            print(order_id)
+            db.session.add(order)
+            for item in cart_items:
+                order_item = OrderItem(order_id=order.id, product_id=item.product_id, quantity=item.quantity)
+                db.session.add(order_item)
+
+            db.session.commit()
+            cart.empty_cart()
+            return redirect(url_for('home'))
         
+
+        # find users cart
+        cart = Cart.query.filter_by(user_id=session['user_id']).first()
+        cart_items = CartItem.query.filter_by(cart_id=cart.id).all()
+        cart_products = []
+        # search payment_details table for user_id
+        payment_details = PaymentDetails.query.filter_by(user_id=session['user_id']).all()
+        payment_form = PaymentForm()
+        for item in cart_items:
+            product = Product.query.get(item.product_id)
+            item = CartDisplay(product.id, product.name, product.price, item.quantity, product.image)
+            cart_products.append(item)
+        return render_template('/checkout.html', title='Checkout', products=cart_products, form=payment_form, payment_details=payment_details)
     
-class CartItem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'), nullable=False)
-
-    def __repr__(self):
-        return ''.join([
-            'CartItem ID: ', str(self.id), '\r\n',
-            'Cart ID: ', str(self.cart_id), '\r\n', str(self.quantity)
-        ])
     
-class CartDisplay():
-    def __init__(self, product_id, name, price, quantity, image) -> None:
-        self.id = product_id
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        self.image = image
-
-class WishList(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
-
-    def __repr__(self):
-        return ''.join([
-            'WishList ID: ', str(self.id), '\r\n',
-            'Product ID: ', str(self.product_id)
-        ])
-
-# Login related classes
-
-class CheckAdmin:
-    def __init__(self, message=None):
-        self.message = message
-
-    def __call__(self, form, field):
-        if field.data.lower() == 'admin':
-            raise ValidationError(self.message)
-
-class BannedChars:
-    def __init__(self, message=None) -> None:
-        self.message = message
-
-    def __call__(self, form, field):
-        banned_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
-        for char in banned_chars:
-            if char in field.data:
-                raise ValidationError(self.message)
-            
+    else:
+        return redirect(url_for('login'))
